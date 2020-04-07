e3cc8a0abbdd290a2f2c975de26b1dfd269a282e
BrunoConti10 <brunitoconti@gmail.com>
2020-04-07 17:48:41 -0300

    Updates

---
+++ precache-manifest.6f61b76a4db410e83c43ad223fada8f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65182e3566c7c7780b465ac6d27b7f0",
+    "url": "/Udemy-Curso-Reactjs/index.html"
+  },
+  {
+    "revision": "d12bdadc70d2e2226d5b",
+    "url": "/Udemy-Curso-Reactjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "49ca47c4f17c7c94d54b",
+    "url": "/Udemy-Curso-Reactjs/static/js/2.5cb85fa8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Udemy-Curso-Reactjs/static/js/2.5cb85fa8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d12bdadc70d2e2226d5b",
+    "url": "/Udemy-Curso-Reactjs/static/js/main.9a32171d.chunk.js"
+  },
+  {
+    "revision": "505162827087e41774fb",
+    "url": "/Udemy-Curso-Reactjs/static/js/runtime-main.8a30ef5a.js"
+  }
+]);
