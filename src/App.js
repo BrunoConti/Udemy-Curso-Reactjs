49cee7647de47f54d7726c909f50a6459f845f26
BrunoConti <bruno.conti1997@gmail.com>
2020-04-15 14:47:51 -0300

    Stateless Component

--- src/App.js
+++ src/App.js
@@ -13,21 +13,34 @@ class Button extends Component {
 
 Button.defaultProps = { borderColor: '#09f' }
 
-class ButtonDanger extends Component {
-  render(){
-    return(<Button borderColor='red' label={this.props.label}></Button>)
-  }
-}
+// class ButtonDanger extends Component {
+//   render(){
+//     return(<Button borderColor='red' label={this.props.label}></Button>)
+//   }
+// }
 
-class ButtonWithLegend extends Component {
-  render(){
-    return(
-      <div>
-        <Button label={this.props.label} borderColor={this.props.borderColor}></Button>
-        <small>{this.props.legend}</small>
-      </div>
-    )
-  }
+// class ButtonWithLegend extends Component {
+//   render(){
+//     return(
+//       <div>
+//         <Button label={this.props.label} borderColor={this.props.borderColor}></Button>
+//         <small>{this.props.legend}</small>
+//       </div>
+//     )
+//   }
+// }
+
+function Article(props) {
+  return(
+    <section>
+      <h2>{props.title}</h2>
+      <p><em>Escrito por {props.author}</em></p>
+      <date>{props.date}</date>
+      <article>
+        {props.children}
+      </article>
+    </section>
+  )
 }
 
 class App extends Component {
@@ -35,13 +48,14 @@ class App extends Component {
   render(){
   return (
     <div className="App">
-      <EjemploDeComponenteWillUnmount/>
-    <br></br>
-      <Button label= 'Clickea aqui!'></Button>
-    <br></br>
-      <ButtonDanger label='Cuidado !!!'></ButtonDanger>
-    <br></br>
-      <ButtonWithLegend label='Boton con leyenda' borderColor='black' legend='Leyenda'></ButtonWithLegend>
+     <h3>Stateless Components</h3>
+     <Article 
+          title='Articulo sobre la prop children' 
+          author='Bruno Conti'
+          date={new Date().toLocaleDateString()}
+          >
+            <p>Aca deberia estar escrito el articulo Aca deberia estar escrito el articulo Aca deberia estar escrito el articulo Aca deberia estar escrito el articulo Aca deberia estar escrito el articulo Aca deberia estar escrito el articulo Aca deberia estar escrito el articulo Aca deberia estar escrito el articulo</p>
+     </Article> 
     </div>
   );
 }
