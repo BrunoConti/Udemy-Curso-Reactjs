fca5f47fd55c2426b58d19ac532afa000d6c4f0b
BrunoConti <bruno.conti1997@gmail.com>
2020-04-15 14:34:10 -0300

    Conditional

--- src/App.js
+++ src/App.js
@@ -3,12 +3,45 @@ import React, { Component } from 'react';
 import EjemploDeComponenteWillUnmount from './sections/life-cycle/ejemploComponentWillUnmount';
 
 
+class Button extends Component {
+  render(){
+    return(
+    <button style={{ borderColor: this.props.borderColor, display: 'block'}}>{this.props.label}</button>
+    )
+  }
+}
+
+Button.defaultProps = { borderColor: '#09f' }
+
+class ButtonDanger extends Component {
+  render(){
+    return(<Button borderColor='red' label={this.props.label}></Button>)
+  }
+}
+
+class ButtonWithLegend extends Component {
+  render(){
+    return(
+      <div>
+        <Button label={this.props.label} borderColor={this.props.borderColor}></Button>
+        <small>{this.props.legend}</small>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   
   render(){
   return (
     <div className="App">
       <EjemploDeComponenteWillUnmount/>
+    <br></br>
+      <Button label= 'Clickea aqui!'></Button>
+    <br></br>
+      <ButtonDanger label='Cuidado !!!'></ButtonDanger>
+    <br></br>
+      <ButtonWithLegend label='Boton con leyenda' borderColor='black' legend='Leyenda'></ButtonWithLegend>
     </div>
   );
 }
